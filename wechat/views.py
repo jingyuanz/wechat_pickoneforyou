30cbc152cb18e18089766e02035c88603eb8eceb
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-13 20:38:22 +0800

    test3

--- wechat/views.py
+++ wechat/views.py
@@ -1,26 +1,37 @@
 #coding=utf-8
 __author__ = 'zhangjingyuan'
-from django.http import HttpResponse
-from django.template import RequestContext
-from django.views.decorators.csrf import csrf_exempt
-import time
 import hashlib
-import wechat_sdk
-from wechat_sdk import WechatBasic
-TOKEN = "jingyuanz"
+import json
+from lxml import etree
+from django.utils.encoding import smart_str
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+TOKEN = 'jingyuanz'
 
 @csrf_exempt
-def wechat(request):
-    wechat = WechatBasic(token=TOKEN)
-    if wechat.check_signature(signature=request.GET['signature'],
-                              timestamp=request.GET['timestamp'],
-                              nonce=request.GET['nonce']):
-        if request.method == 'GET':
-            rsp = request.GET.get('echostr', 'error')
+def weixin_main(request):
+    """
+    所有的消息都会先进入这个函数进行处理，函数包含两个功能，
+    微信接入验证是GET方法，
+    微信正常的收发消息是用POST方法。
+    """
+    if request.method == "GET":
+        signature = request.GET.get("signature", None)
+        timestamp = request.GET.get("timestamp", None)
+        nonce = request.GET.get("nonce", None)
+        echostr = request.GET.get("echostr", None)
+        token = TOKEN
+        tmp_list = [token, timestamp, nonce]
+        tmp_list.sort()
+        tmp_str = "%s%s%s" % tuple(tmp_list)
+        tmp_str = hashlib.sha1(tmp_str).hexdigest()
+        if tmp_str == signature:
+            return HttpResponse(echostr)
         else:
-            wechat.parse_data(request.body)
-            message = wechat.get_message()
-            rsp = wechat.response_text(u'消息类型: {}'.format(message.type))
+            return HttpResponse("weixin  index")
     else:
-        rsp = wechat.response_text('check error')
-    return HttpResponse(rsp)
+        xml_str = smart_str(request.body)
+        request_xml = etree.fromstring(xml_str)
+        response_xml = request_xml
+        return HttpResponse(response_xml)
