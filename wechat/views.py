021caefdcafb6bccc3ddf1f45b9b87d91343d01b
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-13 21:31:07 +0800

    success

--- wechat/views.py
+++ wechat/views.py
@@ -6,7 +6,7 @@ from lxml import etree
 from django.utils.encoding import smart_str
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-
+from django.template import Context,Template,loader
 TOKEN = 'jingyuanz'
 
 @csrf_exempt
@@ -33,5 +33,20 @@ def wechat(request):
     else:
         xml_str = smart_str(request.body)
         request_xml = etree.fromstring(xml_str)
-        response_xml = request_xml
-        return HttpResponse(request_xml)
+        response_xml = parse_message(request_xml)
+        return HttpResponse(response_xml)
+
+
+def parse_message(request_xml):
+    t = loader.get_template('reply_text.xml')
+
+    content=request_xml.find("Content").text#获得用户所输入的内容
+    msgType=request_xml.find("MsgType").text
+    fromUser=request_xml.find("FromUserName").text
+    toUser=request_xml.find("ToUserName").text
+    c = {
+        'toUser' : fromUser,
+        'fromUser' : toUser,
+        'createTime': 1234
+    }
+    return HttpResponse(t.render(c))
