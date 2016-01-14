c8b329dc998299ba866f12ac1d61bddb403e5f72
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-14 14:01:17 +0800

    add choice english

--- wechat/views.py
+++ wechat/views.py
@@ -54,8 +54,8 @@ def parse_message(request_xml):
 
 def parse_content(content):
     if content == u"格式":
-        return "A B C D?X Y Z\nA,B,C,D代表选项, XYZ代表关键词/关键句或条件,用逗号隔开,分别写一行,\n比如\n\n 香蕉 火锅 中药\n好吃 不上火\n\n就能得到科学选择"
-    content = content.split('\n')
+        return "A B C D?X Y Z\nA,B,C,D代表选项, XYZ代表关键词/关键句或条件,用空格隔开,并在两组间用问号隔开,\n比如 香蕉 火锅 中药?好吃 不上火\n就能得到科学选择"
+    content = content.split('?')
     if len(content) != 2:
         return "格式错误, 发送'格式'获取帮助"
     else:
