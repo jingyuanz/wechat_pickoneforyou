615430b15c4dbbc5a2885d0191ffa93ddda66b52
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-14 12:56:47 +0800

    add choice english

--- wechat/views.py
+++ wechat/views.py
@@ -26,7 +26,7 @@ def wechat(request):
         if tmp_str == signature:
             return HttpResponse(echostr, content_type="text/plain")
         else:
-            return None
+            return ""
     elif request.method == "POST":
         xml_str = smart_str(request.body)
         request_xml = etree.fromstring(xml_str)
