ad693242b6a5aa12171e92c6ecf43f1690a2a782
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-14 12:00:19 +0800

    add choice english

--- wechat/views.py
+++ wechat/views.py
@@ -47,20 +47,33 @@ def parse_message(request_xml):
                 <CreateTime>{}</CreateTime>
                 <MsgType><![CDATA[{}]]></MsgType>
                 <Content><![CDATA[{}]]></Content></xml>"""\
-        .format(fromUser, toUser, str(int(time.time())), msgType, "ceshi")
+        .format(fromUser, toUser, str(int(time.time())), msgType, parsed_content)
     return reply
 
 
+# def parse_content(content):
+#     if content == u"格式":
+#         return U"A,B,C,D?X,Y,Z\nA,B,C,D代表选项, XYZ代表关键词/关键句或条件,用逗号隔开,并在两组间用问号隔开, 比如 香蕉,火锅,中药?好吃,不上火. 就能得到科学选择"
+#     content = content.split('?')
+#     if len(content) != 2:
+#         return u"错误的格式, 发送\"格式\"获取帮助"
+#     else:
+#         choices = content[0].split(',')
+#         key_words = content[1].split(',')
+#         if len(choices) < 1 or len(key_words)<1:
+#             return u"格式错误, 发送\"格式\"获取帮助"
+#         else:
+#             return choice(choices)
 def parse_content(content):
-    if content == u"格式":
-        return U"A,B,C,D?X,Y,Z\nA,B,C,D代表选项, XYZ代表关键词/关键句或条件,用逗号隔开,并在两组间用问号隔开, 比如 香蕉,火锅,中药?好吃,不上火. 就能得到科学选择"
+    if content == "geshi":
+        return "GESHI"
     content = content.split('?')
     if len(content) != 2:
-        return u"错误的格式, 发送\"格式\"获取帮助"
+        return "Wrong format"
     else:
         choices = content[0].split(',')
         key_words = content[1].split(',')
-        if len(choices) < 1 or len(key_words)<1:
-            return u"格式错误, 发送\"格式\"获取帮助"
+        if len(choices) <= 1 or len(key_words) <= 1:
+            return "Wrong format"
         else:
-            return choice(choices)
+            return choices[0]
