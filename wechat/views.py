e30451a47534d4013fb6811d91882c35d0463d13
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-14 12:22:15 +0800

    add choice english

--- wechat/views.py
+++ wechat/views.py
@@ -66,7 +66,7 @@ def parse_message(request_xml):
 #             return choice(choices)
 def parse_content(content):
     if content == "geshi":
-        return u"A,B,C,D?X,Y,Z\nA,B,C,D代表选项, XYZ代表关键词/关键句或条件,用逗号隔开,并在两组间用问号隔开, 比如 香蕉,火锅,中药?好吃,不上火. 就能得到科学选择"
+        return "A,B,C,D?X,Y,Z\nA,B,C,D代表选项, XYZ代表关键词/关键句或条件,用逗号隔开,并在两组间用问号隔开, 比如 香蕉,火锅,中药?好吃,不上火. 就能得到科学选择"
     content = content.split('?')
     if len(content) != 2:
         return "Wrong format"
