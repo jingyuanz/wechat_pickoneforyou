149bfad5492fd3c30d4f59e3f87129c799a45460
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-14 13:39:15 +0800

    add choice english

--- wechat/views.py
+++ wechat/views.py
@@ -64,5 +64,5 @@ def parse_content(content):
         if len(choices) <= 1 or len(key_words) <= 1:
             return "格式错误, 发送'格式'获取帮助"
         else:
-            logging.error(choices[0])
-            return "hi"
+            logging.error(choice(choices[0]))
+            return choices[0]
