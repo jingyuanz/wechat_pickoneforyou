b974460fe6965b747d491bbbca25527a6174d904
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-15 15:33:04 +0800

    add search engine

--- wechat/views.py
+++ wechat/views.py
@@ -12,6 +12,8 @@ import time
 import random
 from collections import defaultdict
 from random import choice
+import urllib2
+import re
 TOKEN = 'jingyuanz'
 
 @csrf_exempt
@@ -70,6 +72,10 @@ def parse_message(request_xml):
 #             logging.error(best_choice)
 #             return best_choice
 
+AND = "AND"
+PLUS = '%2B'
+SPACE = '%20'
+QUOTE = '%22'
 def parse_content(content):
     if content == u"格式":
         return "A B C D?X Y Z\nA,B,C,D代表选项, XYZ代表关键词/关键句或条件,用空格隔开,并在两组间用问号隔开,\n比如 香蕉 火锅 中药?好吃 不上火\n就能得到科学选择\n(记住问号是半角英文的问号,目前只是随机)"
@@ -82,20 +88,44 @@ def parse_content(content):
         if len(choices) <= 1 or len(key_words) < 1 or key_words == "":
             return "格式错误, 发送'格式'获取帮助,记住问号'?'一定要是英文的问号!!"
         else:
+            keys = QUOTE+(QUOTE+"AND").join(key_words)+QUOTE
             sum = 0
             content_dict = defaultdict(int)
             for item in choices:
+                item_all_keys = QUOTE+item+QUOTE+AND+keys
+                print item_all_keys
+                penalty = count_search_engine(QUOTE+item+QUOTE)
+                starting_value = 5.0*(count_search_engine(item_all_keys)/penalty)
+                content_dict[item] += starting_value
+                sum += starting_value
                 for key in key_words:
-                    value = random.randint(0,100)
+                    print key
+                    value = 1.0*count_search_engine(convert_into_search_query(item, key))/penalty
                     content_dict[item] += value
                     sum += value
+
             sorted_dict = sorted(content_dict.items(), lambda x, y: cmp(x[1], y[1]), reverse=True)
-            best_choice = sorted_dict[0][0]
-            best_portion = str(round(1.0*sorted_dict[0][1]/sum*100, 2))
+            # best_choice = sorted_dict[0][0]
+            # best_portion = str(round(1.0*sorted_dict[0][1]/sum*100, 2))
             results = ""
             for tup in sorted_dict:
                 results += tup[0] + str(round(1.0*tup[1]/sum*100, 2)) + "%\n\n"
 
-            results += u"综上, 最佳选项是 -- "+best_choice + " "+best_portion+"%"
-            logging.error(results)
+            # results += u"综上, 最佳选项是 -- "+best_choice + u" "+best_portion+u"%"
+            # logging.error(results)
             return results
+
+
+def count_search_engine(content):
+    url_address = 'http://www.baidu.com/s?wd={}'.format(content)
+    f = urllib2.urlopen(url_address)
+    buf = f.read()
+    buf = buf.replace(',', "")
+    num = re.findall(r'百度为您找到相关结果约(\d+)个',buf)
+    if len(num) == 1:
+        return int(num[0])+1
+    return 1
+
+
+def convert_into_search_query(raw1, raw2):
+    return QUOTE+raw1+QUOTE+"AND"+QUOTE+raw2+QUOTE
