0feeaf3770a7895bac1395ee564f68e1326dc770
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-15 16:50:04 +0800

    debugging

--- wechat/views.py
+++ wechat/views.py
@@ -119,7 +119,7 @@ def parse_content(content):
 
 def count_search_engine(content):
     print content
-    print urllib.quote('"白石麻衣"')
+    print urllib.quote(content.truncate())
     url_address = 'http://www.baidu.com/s?wd={}'.format(urllib.quote(content))
     print "a"
     print url_address
