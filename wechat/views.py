8b4a843e61268604a5d68dda335376a721948f06
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-15 17:46:51 +0800

    succeed

--- wechat/views.py
+++ wechat/views.py
@@ -81,7 +81,7 @@ SPACE = ' '
 QUOTE = '"'
 def parse_content(content):
     if content == u"格式":
-        return "A B C D?X Y Z\nABCD代表选项, XYZ代表关键词或条件,用空格隔开,并在两组间用问号隔开,\n比如\n\n 香蕉 火锅 中药?好吃 不上火\n就能得到科学选择\n(记住问号是半角英文的问号)"
+        return "A B C D?X Y Z\nABCD代表选项, XYZ代表关键词或条件,用空格隔开,并在两组间用问号隔开,\n比如\n\n 香蕉 火锅?好吃 不上火\n\n就能得到科学选择\n(记住问号是半角英文的问号)"
     content = content.split('?')
     if len(content) != 2:
         return "格式错误, 发送'格式'获取帮助, 记住问号'?'一定要是英文的问号!! 如有任何BUG或疑问请联系微信号minamotokyon"
