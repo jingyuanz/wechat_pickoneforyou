d55ad745de3573313616f949bab5afeb8444c54e
jingyuanz <barrel.titor.daru@gmail.com>
2016-01-15 16:12:18 +0800

    debugging

--- wechat/views.py
+++ wechat/views.py
@@ -14,6 +14,7 @@ from collections import defaultdict
 from random import choice
 import urllib2
 import re
+import urllib
 TOKEN = 'jingyuanz'
 
 @csrf_exempt
@@ -119,7 +120,7 @@ def parse_content(content):
 
 def count_search_engine(content):
     print "hi"
-    url_address = u'http://www.baidu.com/s?wd={}'.format(urllib2.quote(content))
+    url_address = u'http://www.baidu.com/s?wd={}'.format(urllib.quote(content))
     print url_address
     f = urllib2.urlopen(url_address)
     print 2
